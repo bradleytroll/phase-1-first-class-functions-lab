1c87146d876c3019e8550ea96505b11c7ade02d2
Bradley Troll <bradleytroll@gmail.com>
2023-05-16 09:56:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0, 2)
+};
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(2)
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
+const createFareMultiplier = function(int) {
+    return function(fare) {
+        return int * fare
+    }  
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function(drivers, select) {
+    if (select === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    }
+    else if (select === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers)
+    }
+}
